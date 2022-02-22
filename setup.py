a9c6d0ee1325f8c6b187691591f295b31ee57d94
Evelyne Akopyan <evelyne.akopyan@gmail.com>
2022-02-22 15:22:47 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info < (3, 2):
+    raise Exception('Only Python 3.2+ is supported')
+
+setup(name='swarmpy_simulator',
+      version='0.1',
+      description="Simulator for swarms of nanosatellites",
+      author='Evelyne Akopyan',
+      author_email='evelyne.akopyan@gmail.com',
+      url='https://github.com/eakopyan/SwarmPy-Simulator',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False)
+
+      #packages=find_packages(exclude=['ez_setup', 'examples', 'tests', 'release']),
